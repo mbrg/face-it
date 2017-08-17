ec591cc91835638ef61079206a4d3334bdfcdbf4
Or Golan <orgolan@mail.tau.ac.il>
2017-08-17 16:10:08 +0300

    image preprocessor - done

---
+++ painter/image_preprocessor.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+class ImagePreprocessor:
+    def __init__(self, image, scale_factor=1.1):
+        self.haar_face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
+        self.scale_factor = scale_factor
+        self.image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        self.faces = self.haar_face_cascade.detectMultiScale(self.image,
+                                                             scaleFactor=self.scale_factor,
+                                                             minNeighbors=5)
+
+    def image_cropper(self):
+        # cropping images
+        # faces = (x, y, w, h)
+        face_array = self.faces.flatten()
+        height = face_array[3]
+        width = face_array[2]
+        width, height = max(width, height), max(width, height)
+        cropped = self.image[int(face_array[1] - height * 0.2):int((face_array[1] + height * 1.3)),
+                             int(face_array[0] - width * 0.2):int((face_array[0] + width * 1.3))]
+
+        return cv2.resize(cropped, (512, 512))
