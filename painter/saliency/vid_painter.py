fbffa0951a5ee069ae8ebe45a31fd417f0488f74
mibarg <mibarg@zoho.com>
2017-08-18 03:31:10 +0300

    working fast vid

--- painter/saliency/vid_painter.py
+++ painter/saliency/vid_painter.py
@@ -21,18 +21,29 @@ class Painter(object):
         # init MRF graph
         self.image = image
         self.image_2d = image[:,:,0]  # MRF handles 2D grayscale
+
         self.n, self.m = self.image_2d.shape
-        self.mrf_graph = build_grid_graph(self.n, self.m, self.image_2d, np.zeros((self.n, self.m)))
+        self.obs_mat = np.zeros((self.n, self.m))
+        self.mrf_graph = build_grid_graph(self.n, self.m, self.image_2d, self.obs_mat)
+        #TODO delete if this doesn't cause isues
         self.mrf_graph.init_in_msgs()
 
         # compute saliency
         maps = saliency_map(im=image, is_markMaxima=False)
         self.intensity = cv2.resize(maps.intensity, image_shape)
 
+        blank_img = np.multiply(np.ones((self.n, self.m)), 255).astype(np.int)
+        blank_img[0] = 0
+
+        # changing image versions
+        _='''self.obsed_indecies = []
+        self.observed_image = blank_img
+        self.unobserved_intensity = self.intensity.copy()'''
+
 
     def max_saliency(self):
         # mrf graph to value and observe matrices
-        mat, obs = grid2mat(self.mrf_graph, self.n, self.m, unobserved_to_belief=False)
+        mat, obs = self.image_2d, self.obs_mat
 
         # obscured intensity by observed cells
         self.intensity[(obs == 1)] = self.observed_value
@@ -41,6 +52,28 @@ class Painter(object):
         argmax_coor = np.unravel_index(self.intensity.argmax(), self.intensity.shape)
 
         return self.mrf_graph.get_vertex_by_coor(argmax_coor), self.intensity, obs
+
+    _='''
+    def new_saliency(self):
+        # obs values to nparray
+        self.obsed_indecies = np.asarray(self.obsed_indecies, dtype=int)
+
+        # update obs_mat
+        self.obs_mat[self.obsed_indecies] = 1
+
+        # update unobserved_intensity
+        self.unobserved_intensity[self.obsed_indecies] = self.observed_value
+
+        # update observed image
+        self.observed_image[self.obsed_indecies] = self.image_2d[self.obsed_indecies]
+
+        # forget mem of changes
+        self.obsed_indecies = []
+
+        # argmax coor
+        argmax_coor = np.unravel_index(self.unobserved_intensity.argmax(), self.unobserved_intensity.shape)
+
+        return self.mrf_graph.get_vertex_by_coor(argmax_coor), self.unobserved_intensity, self.obs_mat'''
 
 
     def frame_gen(self):
@@ -56,6 +89,8 @@ class Painter(object):
             if cur_v._is_observed == False:
                 # observe current
                 cur_v._is_observed = True
+                self.obs_mat[cur_v._coor[0], cur_v._coor[1]] = 1
+                #self.obsed_indecies.append(cur_v._coor)
             else:
                 # cicle complete
                 logging.info('Cicle complete. Passed through %d coors' % cntr)
@@ -67,7 +102,7 @@ class Painter(object):
                 logging.info('Moving to coor=%s' % str(next_v._coor if next_v else ''))
 
             if next_v is None:
-                if 0 <= cntr % 1000 <= 100:
+                if 0 <= cntr % 1000 <= 50:
                     logging.info('Jumping to next salient location, no unobserved neighs, number=%d' % cntr)
 
                 # no unobserved neighs
@@ -77,8 +112,9 @@ class Painter(object):
                 cur_image_segment = self.image_2d.copy()
                 cur_image_segment[(cur_obs == 0)] = 255
                 yield cur_image_segment
+                #yield self.observed_image
 
             cur_v = next_v
 
     def min_num_frames(self):
-        return np.count_nonzero(self.intensity==0)
+        return np.count_nonzero(self.intensity)
