52f0cfa70d962bfe85464da8d5d4c8de0a3f20cc
mibarg <mibarg@zoho.com>
2017-08-18 06:58:23 +0300

    web upload page

--- painter/main.py
+++ painter/main.py
@@ -25,9 +25,11 @@ def argument_parser():
     parser.add_argument('--frame_msec', dest='msecs_per_frame', default=6, type=int)
     parser.add_argument('--fps', dest='frames_per_sec', default=None, type=int)
 
-    parser.add_argument('--root', dest='project_root', default='/home/dsteam/repos/face-it/', type=str)
     parser.add_argument('--out', dest='output_path', default='painter/videos/or_vid.mp4', type=str)
     parser.add_argument('--image', dest='image_path', default='painter/examples/or.jpg', type=str)
+    parser.add_argument('--add_root', action='store-true', dest='add_root', default=False, type=str)
+
+    parser.add_argument('--root', dest='project_root', default='/home/dsteam/repos/face-it/', type=str)
     parser.add_argument('--prepro', dest='image_prepro', default='painter/image_preprocessor.py', type=str)
     parser.add_argument('--tmp', dest='tmp_path', default='painter/examples/tmp.jpg', type=str)
     parser.add_argument('--xml', dest='xml_path', type=str, default='painter/data/haarcascade_frontalface_alt.xml')
@@ -42,14 +44,20 @@ def main():
     sys.path.extend([args.project_root, ])
     from painter.saliency.vid_painter import Painter
 
+    # join paths
+    path_image_prepro = join(args.project_root, args.image_prepro)
+    path_image = join(args.project_root,args.image_path) if args.add_root else args.image_path
+    path_out = join(args.project_root, args.output_path) if args.add_root else args.output_path
+    path_tmp = join(args.project_root, args.tmp_path)
+    path_xml = join(args.project_root, args.xml_path)
+
     # read image
     cmnd = "source activate image_pp; %s --image='%s' --grey='%s' --xml='%s'; source deactivate" \
-           % (join(args.project_root, args.image_prepro), join(args.project_root,args.image_path),
-              join(args.project_root, args.tmp_path), join(args.project_root, args.xml_path))
+           % (path_image_prepro, path_image, path_tmp, path_xml)
 
     process = Popen(cmnd, shell=True, stdout=PIPE)
     process.wait()
-    image_segment = cv2.imread(join(args.project_root, args.tmp_path))
+    image_segment = cv2.imread(path_tmp)
 
     # create an MRF and Saliancy agent
     painter = Painter(image_segment, dist_scale_thresh=args.dist_treshold, image_shape=IMAGE_SHAPE, observed_value=OBSERVED_VALUE)
@@ -82,7 +90,7 @@ def main():
     ani = animation.FuncAnimation(fig, updatefig, frames=args.frame_limit, interval=args.msecs_per_frame, blit=True)
     rc('animation', html='html5')
     writer = animation.FFMpegWriter(fps=args.fps)
-    ani.save(args.join(args.project_root, args.output_path), writer=writer)
+    ani.save(path_out, writer=writer)
 
 if __name__ == '__main__':
     main()
--- web-service/app.py
+++ web-service/app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, url_for, redirect, send_from_directory
 #from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
+from os.path import join
+from painter import main
+from subprocess import Popen, PIPE
+
+
+# globals
+UPLOAD_FOLDER = '/home/dsteam/repos/face-it/web-service/tmp/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+FILE_CNTR = 0
 
 #mysql = MySQL()
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 # MySQL configurations
 #app.config['MYSQL_DATABASE_USER'] = 'jay'
@@ -17,9 +28,57 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload_file():
+    """
+    ref: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+    """
+    global FILE_CNTR
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            # save file
+            cnt_filename = '%d.jpg' % FILE_CNTR
+            file.save(join(app.config['UPLOAD_FOLDER'], cnt_filename))
+
+            # generate video
+            cmnd = "source activate image_pp; python '/home/dsteam/repos/face-it/painter/main.py' --image='%s' --out='%s'; source deactivate" \
+                   % (join(UPLOAD_FOLDER, cnt_filename), join(UPLOAD_FOLDER, '%d.mp4' % FILE_CNTR))
+            process = Popen(cmnd, shell=True, stdout=PIPE)
+
+            red = redirect(url_for('uploaded_file', filename=join(UPLOAD_FOLDER, '%d.mp4' % FILE_CNTR)))  # redirects to /uploads/filename
+
+            FILE_CNTR += 1
+            return red
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <p><input type=file name=file>
+             <input type=submit value=Upload>
+        </form>
+        '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == "__main__":
