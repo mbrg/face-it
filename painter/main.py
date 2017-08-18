6b889dbf5957547295983975485ccb93da518c0c
Or Golan <orgolan@mail.tau.ac.il>
2017-08-18 07:06:16 +0300

    matplotlib is shit

--- painter/main.py
+++ painter/main.py
@@ -63,6 +63,7 @@ def main():
     blank_img = np.multiply(np.ones(IMAGE_SHAPE), 255).astype(np.int)
     blank_img[0] = 0
     im = plt.figimage(blank_img, 'gray', animated=True)
+    plt.close(fig)
     cur_frame, frame_cntr = None, 0
 
     # define frame producing function
